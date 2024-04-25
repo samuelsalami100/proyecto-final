7aab05602c184e5d1d07e265bea0719dd72f56aa
Samuel <samuelsalami100@gmail.com>
2024-04-25 02:31:49 +0200

    Timer

--- script.js
+++ script.js
@@ -28,23 +28,49 @@ const coleccionPalabras = ['Árbol', 'Bicicleta', 'Caja',
     'Zorro']
 
 
+/** @type {HTMLButtonElement} */
+const btnInicio = document.querySelector('.btn-inicio')
+
+/** @type {HTMLDivElement} */
+const timer = document.querySelector('.timer')
+
+let intervalId = -1
+
+
 /** @type {string[]} */
 let palabras = []
 ////////////////////////////////////////////////////////////////////////////////
 function iniciarJuego() {
     // Desordenar y quedarnos con 9
-    coleccionPalabras.sort( () => 0.5 - Math.random())
+    coleccionPalabras.sort( () => 0.5 - Math.random() )
     palabras = coleccionPalabras.slice(0, 9)
 
-    
+    const divPalabras = document.querySelectorAll('.tablero__palabra')
+    divPalabras.forEach((e, i)=>{
+        e.textContent = palabras[i]
+    })
+
+    btnInicio.style.display = 'none'
+    timer.style.display = 'flex'
+
+    let time = 3
+    timer.textContent = time
+    intervalId = setInterval(()=>{
+        timer.textContent = time
+        if (time == 0) {
+            clearInterval(intervalId)
+        }
+        time--
+
+    }, 1000)
 }
+
+
 ////////////////////////////////////////////////////////////////////////////////
 
 
 
-/** @type {HTMLButtonElement} */
-const btn = document.querySelector('.btn-inicio')
 
-btn.addEventListener('pointerdown', () => {
+btnInicio.addEventListener('pointerdown', () => {
     iniciarJuego()
 })
